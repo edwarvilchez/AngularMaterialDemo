44032235f1c1559ee084cf96a442096451ce0c1e
edwarvilchez <edwarvilchez1977@gmail.com>
2020-09-14 17:15:12 -0400

    Se agregan los botones, sus propiedades y estilos

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material';
+import { MatDividerModule } from '@angular/material';
+import { MatIconModule } from '@angular/material';
 
 
 
 //declaramos una constante para usar los componentes de material
 const MaterialComponents = [
-	MatButtonModule
+	MatButtonModule,
+	MatDividerModule,
+	MatIconModule
 ]
 
 
